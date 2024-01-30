7a86fd706927e117ca4b1c3052ebf23837175f27
Clai-van69 <93194645+Clai-van69@users.noreply.github.com>
2024-01-30 08:33:30 +0000

    feedback GEE

--- db/create.sql
+++ db/create.sql
@@ -2,6 +2,11 @@
 -- create tables
 --
 
+-- feebdakc GEE 30 jan 2024
+-- jullie hebben pas 1 tabel;, kijk naar de beoordelingscriteria in de opdracth hoeveel je erbij moet maken
+-- er staat een video op de opdracht die je helpt met een 1:n rellatie (bijvoorrbeeld voor een table met merken)
+
+
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
@@ -24,7 +29,7 @@ insert into products (name, description, code, price) values ('Rolex', 'Mauris e
 insert into products (name, description, code, price) values ('Patek Phillipe', 'De Patek Aquanaut, een modern sporthorloge', '077030122-3', 11);
 insert into products (name, description, code, price) values ('Cartier', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
 insert into products (name, description, code, price) values ('Audemars Piguet', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Tag Heuer, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
+insert into products (name, description, code, price) values ('Tag Heuer', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('Breitling', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 insert into products (name, description, code, price) values ('Omega', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 insert into products (name, description, code, price) values ('Tudor', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
